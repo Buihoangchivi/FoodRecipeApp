567a79bfc39086a15b52aebbb771cb2df744b4d8
Vĩ Bùi <buivankhang9@gmail.com>
2020-10-29 14:15:41 +0700

    - Tạo thanh điều hướng điều khiển phía bên trái giao diện gồm các nút Home, Favorite, Add, Ingredient. - Tạo thanh điều hướng loại đồ ăn ở phía trên giao diện gồm All, Food, Drinks. - Tạo ô tìm kiếm, tạo 2 nút xóa và tìm kiếm dữ liệu ở góc trên bên phải giao diện. - Tạo lưới 3 x 4 để hiển thị hình ảnh và tên thức ăn. - Tạo nút Menu ở góc trên bên trái màn hình để thu gọn hoặc phóng to thanh điều hướng điều khiển. - Khi thu gọn menu thì chỉ hiển thị các hình ảnh icon chứ không hiển thị tên icon, đồng thời kích thước lưới tăng lên thành 3 x 5 để hiển thị nhiều thức ăn hơn. - Mỗi ô thức ăn đều có hiển thị hình trái tim để đánh dấu món ăn yêu thích. Nếu muốn thêm vào danh sách yêu thích thì nhấn vào hình trái tim thì trái tim sẽ chuyển từ màu trắng sang màu đỏ và ngược lại.

--- FoodRecipeApp/MainWindow.xaml.cs
+++ FoodRecipeApp/MainWindow.xaml.cs
@@ -20,9 +20,169 @@ namespace FoodRecipeApp
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private Button clickedTypeButton, clickedControlButton;
+		private bool isFavorite, checkFavoriteIsClicked, isMinimizeMenu;
+		private Rectangle[] imageFoodArray;
+		private TextBlock[] foodNameArray;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+
+			imageFoodArray = new Rectangle[] {
+				Image_0, Image_1, Image_2, Image_3,
+				Image_4, Image_5, Image_6, Image_7,
+				Image_8, Image_9, Image_10, Image_11,
+				Image_12, Image_13, Image_14};
+
+			foodNameArray = new TextBlock[] {
+				FoodName_0, FoodName_1, FoodName_2, FoodName_3,
+				FoodName_4, FoodName_5, FoodName_6, FoodName_7,
+				FoodName_8, FoodName_9, FoodName_10, FoodName_11,
+				FoodName_12, FoodName_13, FoodName_14};
+
+			isFavorite = false;
+			checkFavoriteIsClicked = false;
+			isMinimizeMenu = false;
+
+			//Default buttons
+			clickedTypeButton = AllButton;
+			clickedTypeButton.Background = Brushes.LightSkyBlue;
+			clickedControlButton = HomeButton;
+			clickedControlButton.Background = Brushes.LightSkyBlue;
+		}
+
+		private void changeClickedTypeButton(Button button)
+		{
+			//clickedTypeButton.Background = Brushes.LightGray;
+			//clickedTypeButton = button;
+			//button.Background = Brushes.LightSkyBlue;
+		}
+
+		private void changeClickedControlButton(Button button)
+		{
+			//clickedControlButton.Background = Brushes.LightGray;
+			//clickedControlButton = button;
+			//button.Background = Brushes.LightSkyBlue;
+		}
+
+		private void HomeButton_Click(object sender, RoutedEventArgs e)
+		{
+			changeClickedControlButton(HomeButton);
+		}
+
+		private void FavoriteButton_Click(object sender, RoutedEventArgs e)
+		{
+			changeClickedControlButton(FavoriteButton);
+		}
+
+		private void AddDishButton_Click(object sender, RoutedEventArgs e)
+		{
+			changeClickedControlButton(AddDishButton);
+		}
+
+		private void IngredientButton_Click(object sender, RoutedEventArgs e)
+		{
+			changeClickedControlButton(IngredientButton);
+		}
+
+		private void AllButton_Click(object sender, RoutedEventArgs e)
+		{
+			changeClickedTypeButton(AllButton);
+		}
+
+		private void FoodButton_Click(object sender, RoutedEventArgs e)
+		{
+			changeClickedTypeButton(FoodButton);
+		}
+		private void DrinksButton_Click(object sender, RoutedEventArgs e)
+		{
+			changeClickedTypeButton(DrinksButton);
+		}
+
+		private void Menu_Click(object sender, RoutedEventArgs e)
+		{
+			if (isMinimizeMenu == false)
+			{
+				ControlStackPanel.Width = 47;
+				FoodUniformGrid.Columns = 5;
+				for (int i = 0; i < imageFoodArray.Length - 3; i++)
+				{
+					imageFoodArray[i].Width = 190;
+					foodNameArray[i].Width = 169;
+				}
+				Food_12.Visibility = Visibility.Visible;
+				Food_13.Visibility = Visibility.Visible;
+				Food_14.Visibility = Visibility.Visible;
+				//Image_0_0.Fill = new ImageBrush(new BitmapImage(
+				//	new Uri("Images/playerImage04.jpg", UriKind.Relative)));
+				isMinimizeMenu = true;
+			}
+			else
+			{
+				ControlStackPanel.Width = 200;
+				FoodUniformGrid.Columns = 4;
+				for (int i = 0; i < imageFoodArray.Length - 3; i++)
+				{
+					imageFoodArray[i].Width = 202;
+					foodNameArray[i].Width = 181;
+				}
+				Food_12.Visibility = Visibility.Collapsed;
+				Food_13.Visibility = Visibility.Collapsed;
+				Food_14.Visibility = Visibility.Collapsed;
+				isMinimizeMenu = false;
+			}
+		}
+
+		private void FoodImage_Click(object sender, RoutedEventArgs e)
+		{
+			if (checkFavoriteIsClicked == false)
+			{
+				//var bitmap = new BitmapImage(
+				//	new Uri(
+				//		"Images/playerImage04.jpg",
+				//		UriKind.Relative)
+				//);
+				//Button button = (Button)sender;
+				//string btnName = button.Name;
+				//int index = (btnName[btnName.Length - 3] - '0') * 4 + btnName[btnName.Length - 1] - '0';
+				//imageFoodArray[index].ImageSource = bitmap;
+			}
+			else
+			{
+				checkFavoriteIsClicked = false;
+			}
+		}
+
+		private void Favorite_Click(object sender, RoutedEventArgs e)
+		{
+			checkFavoriteIsClicked = true;
+			string imgName;
+			//Nếu chưa yêu thích thì chuyển sang ảnh yêu thích và thêm vào danh sách yêu thích
+			if (isFavorite == true)
+			{
+				imgName = "Images/unloved.png";
+				isFavorite = false;
+			}
+			else //Nếu yêu thích rồi chuyển sang ảnh chưa yêu thích và xóa khỏi danh sách yêu thích
+			{
+				imgName = "Images/favorite.png";
+				isFavorite = true;
+			}
+
+			//Lấy nguồn ảnh
+			Image img = new Image
+			{
+				Source = new BitmapImage(new Uri(
+						imgName,
+						UriKind.Relative)
+				)
+			};
+
+			//Thay đổi ảnh
+			Button button = (Button)sender;
+			button.Content = img;
 		}
 	}
 }
